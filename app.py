4f5fa13c1fa3ef6f7f27403023eeaceee95ce29a
Tuankhoa382 <nguyendangkhoa420614@gmail.com>
2025-10-24 09:56:25 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+﻿from flask import Flask, render_template
+
+# Tạo ứng dụng Flask
+app = Flask(__name__)
+
+# Định nghĩa route (đường dẫn trang chủ)
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# Chạy ứng dụng
+if __name__ == "__main__":
+    app.run(debug=True)
